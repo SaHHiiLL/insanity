5e272640ec177dade6a7ca3f49f091dd0d2af5fb
Sahil <sahilpanchal2903@gmail.com>
2025-02-24 15:56:40 +0000

    lexer complete, just have to tink of using the language2

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,12 +6,9 @@
 *
 */
 
-use std::time::Instant;
-
-use iter_tools::Itertools;
-
 use super::peaker::Peaker;
 
+#[derive(Debug, PartialEq, Eq)]
 enum Token {
     Identifier(String),
     Number(i64),
@@ -36,12 +33,25 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    // TODO: change this to a two pointer approach and return a string slice
+    fn skip_spaces(&mut self) {
+        loop {
+            if let Some(x) = self.input.get() {
+                if x.is_whitespace() {
+                    self.input.next();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
     fn read_identifer(&mut self) -> Option<String> {
         let start_idx = self.input.idx();
         let mut end_idx = start_idx;
 
-        while self.input.get()?.is_ascii_alphabetic() {
+        while self.input.get()?.is_alphanumeric() {
             end_idx = self.input.idx();
             self.input.next()?;
         }
@@ -54,13 +64,21 @@ impl<'a> Lexer<'a> {
         )
     }
 
-    fn read_identifer2(&mut self) -> Option<String> {
-        let mut buffer = String::new();
-        while self.input.get()?.is_ascii_alphabetic() {
-            buffer.push(*self.input.get()?);
+    fn read_number(&mut self) -> Option<String> {
+        let start_idx = self.input.idx();
+        let mut end_idx = start_idx;
+
+        while self.input.get()?.is_numeric() {
+            end_idx = self.input.idx();
             self.input.next()?;
         }
-        Some(buffer)
+        Some(
+            self.input
+                .slice_x_y(start_idx, end_idx)?
+                .iter()
+                .cloned()
+                .collect::<String>(),
+        )
     }
 }
 
@@ -68,20 +86,22 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let ident;
+        self.skip_spaces();
         let res = match self.input.next()? {
             'a'..='z' | 'A'..='Z' => {
-                let i = Instant::now();
-                ident = self.read_identifer()?;
-                eprintln!("Time: {}", i.elapsed().as_nanos());
-                Some(Token::Identifier(dbg!(ident)))
+                let ident = self.read_identifer()?.to_string();
+                Some(Token::Identifier(dbg!(ident.to_string())))
             }
-            '0'..='9' => todo!("Number"),
+            '0'..='9' => {
+                let ident = self.read_number()?.to_string();
+                Some(Token::Identifier(dbg!(ident.to_string())))
+            }
             ')' => Some(Token::RightParen),
             '(' => Some(Token::LeftParen),
             '}' => Some(Token::RightBrace),
             '{' => Some(Token::LeftBrace),
             '=' => Some(Token::Equals),
+            ',' => Some(Token::Comma),
             '\0' => None,
             _ => None,
         };
@@ -94,7 +114,7 @@ mod tests {
 
     #[test]
     fn test_lexer() {
-        let input = "(kkkkkkkkkkkkkkkkkkkkkkkkjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhsd){(sa}}=".chars().collect::<Vec<char>>();
+        let input = "(){()}=".chars().collect::<Vec<char>>();
         let lexer = Lexer::new(&input);
         let expected = &[
             Token::LeftParen,
@@ -107,6 +127,7 @@ mod tests {
         ];
 
         let lexed_tokens: Vec<Token> = lexer.collect();
-        // assert_eq!(lexed_tokens.len(), expected.len());
+        assert_eq!(lexed_tokens.len(), expected.len());
+        assert_eq!(lexed_tokens, expected);
     }
 }
