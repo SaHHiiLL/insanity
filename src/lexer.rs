363fa71399089906c7986aa4a4f28461518efa13
Sahil <sahilpanchal2903@gmail.com>
2025-02-24 12:15:45 +0000

    Initial draft

---
+++ src/lexer.rs
@@ -0,0 +1,95 @@
+/*
+*
+* project(<NAME>, <VERSION>, [DESCRIPTION])
+*
+* define_library(<LIB_NAME>, path=)
+*
+*/
+
+enum Token<'a> {
+    Identifier(&'a str),
+    Number(i64),
+    RightParen,
+    LeftParen,
+    RightBrace,
+    LeftBrace,
+    Comma,
+    StringLiteral(&'a str),
+    Equals,
+    NotEquals,
+}
+
+struct Lexer<'a> {
+    input: &'a [char],
+    position: usize,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(input: &'a [char]) -> Self {
+        Self { input, position: 0 }
+    }
+
+    fn can_walk(&mut self) -> bool {
+        self.position < self.input.len()
+    }
+
+    fn walk(&mut self) {
+        self.position += 1;
+    }
+
+    fn curr_char(&mut self) -> char {
+        self.input[self.position]
+    }
+
+    fn peak(&self) -> char {
+        self.input[self.position + 1]
+    }
+
+    fn read_identifer() {}
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.can_walk() {
+            return None;
+        }
+        let res = match self.curr_char() {
+            'a'..='z' | 'A'..='Z' => todo!("Identifier"),
+            '0'..='9' => todo!("Number"),
+            ')' => Some(Token::RightParen),
+            '(' => Some(Token::LeftParen),
+            '}' => Some(Token::RightBrace),
+            '{' => Some(Token::LeftBrace),
+            '=' => Some(Token::Equals),
+            '\0' => None,
+            _ => None,
+        };
+        self.walk();
+        res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lexer() {
+        let input = "(){()}=".chars().collect::<Vec<char>>();
+        let lexer = Lexer::new(&input);
+        let expected = &[
+            Token::LeftParen,
+            Token::RightParen,
+            Token::LeftBrace,
+            Token::LeftParen,
+            Token::RightParen,
+            Token::RightBrace,
+            Token::Equals,
+        ];
+
+        let lexed_tokens: Vec<Token> = lexer.collect();
+        assert_eq!(lexed_tokens.len(), expected.len());
+    }
+}
