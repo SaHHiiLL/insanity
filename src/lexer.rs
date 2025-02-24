5791c804ce9b61253e1c0603150e3f3c4891c7ea
Sahil <sahilpanchal2903@gmail.com>
2025-02-24 14:51:16 +0000

    something

--- src/lexer.rs
+++ src/lexer.rs
@@ -6,41 +6,76 @@
 *
 */
 
+use std::time::Instant;
+
+use iter_tools::Itertools;
+
 use super::peaker::Peaker;
 
-enum Token<'a> {
-    Identifier(&'a str),
+enum Token {
+    Identifier(String),
     Number(i64),
     RightParen,
     LeftParen,
     RightBrace,
     LeftBrace,
     Comma,
-    StringLiteral(&'a str),
+    StringLiteral(String),
     Equals,
     NotEquals,
 }
 
 struct Lexer<'a> {
     input: Peaker<'a, char>,
-    position: usize,
 }
 
 impl<'a> Lexer<'a> {
     fn new(input: &'a [char]) -> Self {
         Self {
             input: Peaker::new(input),
-            position: 0,
         }
+    }
+
+    // TODO: change this to a two pointer approach and return a string slice
+    fn read_identifer(&mut self) -> Option<String> {
+        let start_idx = self.input.idx();
+        let mut end_idx = start_idx;
+
+        while self.input.get()?.is_ascii_alphabetic() {
+            end_idx = self.input.idx();
+            self.input.next()?;
+        }
+        Some(
+            self.input
+                .slice_x_y(start_idx, end_idx)?
+                .iter()
+                .cloned()
+                .collect::<String>(),
+        )
+    }
+
+    fn read_identifer2(&mut self) -> Option<String> {
+        let mut buffer = String::new();
+        while self.input.get()?.is_ascii_alphabetic() {
+            buffer.push(*self.input.get()?);
+            self.input.next()?;
+        }
+        Some(buffer)
     }
 }
 
 impl<'a> Iterator for Lexer<'a> {
-    type Item = Token<'a>;
+    type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let ident;
         let res = match self.input.next()? {
-            'a'..='z' | 'A'..='Z' => todo!("Identifier"),
+            'a'..='z' | 'A'..='Z' => {
+                let i = Instant::now();
+                ident = self.read_identifer()?;
+                eprintln!("Time: {}", i.elapsed().as_nanos());
+                Some(Token::Identifier(dbg!(ident)))
+            }
             '0'..='9' => todo!("Number"),
             ')' => Some(Token::RightParen),
             '(' => Some(Token::LeftParen),
@@ -54,13 +89,12 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
-#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_lexer() {
-        let input = "(){()}=".chars().collect::<Vec<char>>();
+        let input = "(kkkkkkkkkkkkkkkkkkkkkkkkjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhjhsd){(sa}}=".chars().collect::<Vec<char>>();
         let lexer = Lexer::new(&input);
         let expected = &[
             Token::LeftParen,
@@ -73,6 +107,6 @@ mod tests {
         ];
 
         let lexed_tokens: Vec<Token> = lexer.collect();
-        assert_eq!(lexed_tokens.len(), expected.len());
+        // assert_eq!(lexed_tokens.len(), expected.len());
     }
 }
--- src/peaker.rs
+++ src/peaker.rs
@@ -1,19 +1,41 @@
 pub(crate) struct Peaker<'a, T> {
     items: &'a [T],
-    curr: usize,
+    idx: usize,
 }
 
 impl<'a, T> Peaker<'a, T> {
     pub(crate) fn new(items: &'a [T]) -> Self {
-        Self { items, curr: 0 }
+        Self { items, idx: 0 }
     }
 
     pub(crate) fn peak(&self) -> Option<&T> {
-        self.items.get(self.curr + 1)
+        self.items.get(self.idx + 1)
     }
 
-    pub(crate) fn curr(&self) -> Option<&T> {
-        self.items.get(self.curr)
+    pub(crate) fn idx(&self) -> usize {
+        self.idx
+    }
+
+    pub(crate) fn get(&self) -> Option<&T> {
+        self.items.get(self.idx)
+    }
+
+    pub(crate) fn slice_x_y(&self, x: usize, y: usize) -> Option<&[T]> {
+        if x >= self.items.len() {
+            return None;
+        }
+        if y >= self.items.len() {
+            return None;
+        }
+        Some(&self.items[x..=y])
+    }
+
+    pub(crate) fn slice_n(&self, n: usize) -> Option<&[T]> {
+        if n >= self.items.len() {
+            None
+        } else {
+            Some(&self.items[self.idx..=n])
+        }
     }
 }
 
@@ -21,8 +43,8 @@ impl<'a, T> Iterator for Peaker<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let res = self.items.get(self.curr)?;
-        self.curr += 1;
+        let res = self.items.get(self.idx)?;
+        self.idx += 1;
         Some(res)
     }
 }
